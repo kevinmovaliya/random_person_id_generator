564a5030656acdfd16cdb48f202dc2634664ab68
kevinmovaliya <kevinmovaliya62gmail.com>
2022-09-27 13:14:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:random_person_id_generator/utils/appRoutes.dart';
+
+import 'utils/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: HomePage(),
+      initialRoute: AppRoutes().homePage,
+      routes: routes,
+    );
+  }
+}
